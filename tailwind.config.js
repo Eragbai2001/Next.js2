9316b7de7a610154e5cca83094fc3c36b98ed0c4
Josiah Aideloje <aideloje.josiah@lmu.edu.ng>
2024-09-21 14:41:19 +0100

    Recent Changes

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        lusitana: ["Lusitana", "serif"], // Add your font here
+      },
+    },
+  },
+  plugins: [],
+};
